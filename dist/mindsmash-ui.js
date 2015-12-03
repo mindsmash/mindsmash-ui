bff151e37a044498b3517a00d540397580b2eb99
Jan Marquardt <jan.marquardt@mindsmash.com>
2015-12-03 09:16:11 +0100

    build

--- dist/mindsmash-ui.js
+++ dist/mindsmash-ui.js
@@ -1,4 +1,4 @@
-angular.module('msm.components.ui', ['ui.router', 'pascalprecht.translate']);
+angular.module('msm.components.ui', ['ui.router', 'pascalprecht.translate', 'ui-notification']);
 
 (function() {
   'use strict';
@@ -45,8 +45,6 @@ angular.module('msm.components.ui', ['ui.router', 'pascalprecht.translate']);
   }
 })();
 
-angular.module("msm.components.ui").run(["$templateCache", function($templateCache) {$templateCache.put("components/ui/msm-button/msm-button.html","<button type=\"button\" ng-class=\"{\'is-msm-mobile-menu-item\': isMobileMenuItem}\" class=\"btn {{ btnClass }}\">\n  <i class=\"mr-0 {{ iconClass }}\"></i>\n  <span>{{ text }}</span>\n</button>\n");
-$templateCache.put("components/ui/msm-mobile-menu-item/msm-mobile-menu-item.html","<div class=\"msm-mobile-menu-item\">\n  <i ng-class=\"icon\" class=\"left-icon\"></i>\n	<div class=\"menu-label\">{{ labelText }}</div>\n	<div class=\"preview-value\">{{ previewValue }}</div>\n	<i class=\"icon-arrow-right\" data-ng-click=\"goToState()\"></i>\n</div>\n");}]);
 (function() {
 	'use strict';
 
@@ -89,4 +87,87 @@ $templateCache.put("components/ui/msm-mobile-menu-item/msm-mobile-menu-item.html
 			}
 		};
 	}
+})();
+
+angular.module("msm.components.ui").run(["$templateCache", function($templateCache) {$templateCache.put("components/ui/msm-button/msm-button.html","<button type=\"button\" ng-class=\"{\'is-msm-mobile-menu-item\': isMobileMenuItem}\" class=\"btn {{ btnClass }}\">\n  <i class=\"mr-0 {{ iconClass }}\"></i>\n  <span>{{ text }}</span>\n</button>\n");
+$templateCache.put("components/ui/msm-mobile-menu-item/msm-mobile-menu-item.html","<div class=\"msm-mobile-menu-item\">\n  <i ng-class=\"icon\" class=\"left-icon\"></i>\n	<div class=\"menu-label\">{{ labelText }}</div>\n	<div class=\"preview-value\">{{ previewValue }}</div>\n	<i class=\"icon-arrow-right\" data-ng-click=\"goToState()\"></i>\n</div>\n");}]);
+(function() {
+    'use strict';
+
+    /**
+     * A service for raising notifications.
+     *
+     * This service wraps all methods provided by the angular-ui-notification service and translates the
+     * message key if needed.
+     *
+     * Provided message functions:
+     *   * primary  - displays a primary notification
+     *   * error    - displays an error notification
+     *   * success  - displays a success notification
+     *   * info     - displays an info notification
+     *   * warning  - displays a warning notification
+     *   * clearAll - clears all notifications that are currently displayed
+     *
+     * Usage:
+     * msmNotification.<function>('i18nKey'); // use i18n
+     * msmNotification.<function>('i18nKey', true); // use i18n
+     * msmNotification.<function>('i18nKey', false); // don't use i18n
+     * msmNotification.<function>('i18nKey', { i18nArg: 'someArg' }); // use i18n with i18n options
+     * msmNotification.<function>({ message: 'i18nKey', delay: 1000 }); // use 18n with notify options
+     * msmNotification.<function>({ message: 'i18nKey', delay: 1000 }, { i18nArg: 'someArg' }); // use 18n with i18n & notify options
+     */
+    angular
+        .module('msm.components.ui')
+        .factory('msmNotification', NotificationService);
+
+    function NotificationService($translate, Notification) {
+        var doFlash = function(args, i18n, notify) {
+            if (typeof args !== 'object') {
+                args = {
+                    message : args
+                };
+            }
+
+            if (i18n === false) {
+                notify(args);
+            } else {
+                $translate(args.message, i18n === true ? undefined : i18n).then(function(msg) {
+                    notify(angular.extend(args, { message: msg }));
+                }, function(msg) {
+                    notify(angular.extend(args, { message: msg }));
+                });
+            }
+        };
+        return {
+            primary : function(args, i18n) {
+                doFlash(args, i18n, function(args) {
+                    Notification.primary(args);
+                });
+            },
+            error : function(args, i18n) {
+                doFlash(args, i18n, function(args) {
+                    Notification.error(args);
+                });
+            },
+            success : function(args, i18n) {
+                doFlash(args, i18n, function(args) {
+                    Notification.success(args);
+                });
+            },
+            info : function(args, i18n) {
+                doFlash(args, i18n, function(args) {
+                    Notification.info(args);
+                });
+            },
+            warning : function(args, i18n) {
+                doFlash(args, i18n, function(args) {
+                    Notification.warning(args);
+                });
+            },
+            clearAll : function() {
+                Notification.clearAll();
+            }
+        };
+    }
+
 })();
