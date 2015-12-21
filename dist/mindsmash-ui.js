bc03186101ca5fe6b86c5bec306f19f364be3fb8
Fynn Feldpausch <fynn.feldpausch@mindsmash.com>
2015-12-21 15:16:48 +0100

    build files

--- dist/mindsmash-ui.js
+++ dist/mindsmash-ui.js
@@ -190,6 +190,58 @@ angular.module('msm.components.ui')
 	}
 })();
 
+(function () {
+  'use strict';
+
+  /**
+   * @ngdoc directive
+   * @name components.ui.msmEditableText
+   * @restrict 'A'
+   *
+   * @description Adds functionality to conditionally display text only to input elements.
+   *
+   * @param {expression} msmEditableText Shows the edit view if the expression is truthy.
+   * @param {expression} msmEditableDisplay The display value in non-edit view.
+   *                     The value of ngModel will be used in absence of this value.
+   */
+  angular.module('msm.components.ui')
+      .directive('msmEditableText', EditableText);
+
+  function EditableText($compile) {
+    return {
+      restrict: 'A',
+      require: '?ngModel',
+      scope: {
+        edit: '=msmEditableText',
+        value: '=msmEditableDisplay'
+      },
+      compile: function compile(tElem, tAttrs) {
+        var tText = $compile('<p ng-show="!edit" class="form-control-static">{{ value }}</p>');
+        return {
+          post: function postLink(scope, iElem, iAttrs, ctrl) {
+            iElem.after(tText(scope));
+
+            // setup watch on ngModel in absence of msm-editable-display
+            if (angular.isUndefined(iAttrs.msmEditableDisplay) && ctrl) {
+              scope.$watch(function () {
+                return ctrl.$modelValue;
+              }, function(newVal, oldVal) {
+                scope.value = newVal;
+              });
+            }
+
+            // setup watch to show / hide input element
+            scope.$watch('edit', function(newVal, oldVal) {
+              iElem.attr('style', newVal ? '' : 'display: none');
+            });
+          }
+        }
+      }
+    }
+  }
+
+})();
+
 /** Based on https://github.com/GabiGrin/angular-editable-text */
 (function () {
   'use strict';
@@ -347,58 +399,6 @@ angular.module('msm.components.ui')
           lastValue = newVal;
           scope.editingValue = newVal;
         });
-      }
-    }
-  }
-
-})();
-
-(function () {
-  'use strict';
-
-  /**
-   * @ngdoc directive
-   * @name components.ui.msmEditableText
-   * @restrict 'A'
-   *
-   * @description Adds functionality to conditionally display text only to input elements.
-   *
-   * @param {expression} msmEditableText Shows the edit view if the expression is truthy.
-   * @param {expression} msmEditableDisplay The display value in non-edit view.
-   *                     The value of ngModel will be used in absence of this value.
-   */
-  angular.module('msm.components.ui')
-      .directive('msmEditableText', EditableText);
-
-  function EditableText($compile) {
-    return {
-      restrict: 'A',
-      require: '?ngModel',
-      scope: {
-        edit: '=msmEditableText',
-        value: '=msmEditableDisplay'
-      },
-      compile: function compile(tElem, tAttrs) {
-        var tText = $compile('<p ng-show="!edit" class="form-control-static">{{ value }}</p>');
-        return {
-          post: function postLink(scope, iElem, iAttrs, ctrl) {
-            iElem.after(tText(scope));
-
-            // setup watch on ngModel in absence of msm-editable-display
-            if (angular.isUndefined(iAttrs.msmEditableDisplay) && ctrl) {
-              scope.$watch(function () {
-                return ctrl.$modelValue;
-              }, function(newVal, oldVal) {
-                scope.value = newVal;
-              });
-            }
-
-            // setup watch to show / hide input element
-            scope.$watch('edit', function(newVal, oldVal) {
-              iElem.attr('style', newVal ? '' : 'display: none');
-            });
-          }
-        }
       }
     }
   }
