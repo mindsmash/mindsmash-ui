e65d2d11a77dc6ef796128794c2c038575187166
Jan Marquardt <jan.marquardt@mindsmash.com>
2016-03-09 22:40:15 +0100

    New build

--- docs/scripts/mindsmash-ui.js
+++ docs/scripts/mindsmash-ui.js
@@ -34,6 +34,29 @@ angular.module('msm.components.ui')
   uibDatepickerPopupConfig.datepickerPopupTemplateUrl = 'components/ui/msm-datepicker/msm-datepicker-popup.html';
 });
 
+angular.module('msm.components.util')
+.directive('scrollLink', ScrollLink);
+
+/**
+ * @ngdoc directive
+ * @name components.util:ScrollLink
+ * @scope
+ * @restrict 'E'
+ *
+ * @description
+ *    Directive that renders a link to jump to via #hash in url.
+ */
+function ScrollLink() {
+  return {
+    template: '<a href="#{{ name }}" id="{{ name }}" class="scroll-link"></a>',
+    scope      : {
+      name: '@'
+    }
+  };
+}
+
+;
+
 (function () {
   'use strict';
 
@@ -87,29 +110,6 @@ angular.module('msm.components.ui')
     };
   }
 })();
-
-angular.module('msm.components.util')
-.directive('scrollLink', ScrollLink);
-
-/**
- * @ngdoc directive
- * @name components.util:ScrollLink
- * @scope
- * @restrict 'E'
- *
- * @description
- *    Directive that renders a link to jump to via #hash in url.
- */
-function ScrollLink() {
-  return {
-    template: '<a href="#{{ name }}" id="{{ name }}" class="scroll-link"></a>',
-    scope      : {
-      name: '@'
-    }
-  };
-}
-
-;
 
 (function() {
   'use strict';
