e332b848c141e98b5735174329d99c04e956d7ab
Fynn Feldpausch <fynn.feldpausch@mindsmash.com>
2016-04-14 15:32:41 +0200

    fixed style bug in nav-icons class

--- docs/scripts/mindsmash-ui.js
+++ docs/scripts/mindsmash-ui.js
@@ -458,6 +458,53 @@ angular.module('msm.components.ui')
   }
 })();
 
+(function() {
+	'use strict';
+
+	/**
+	 * @ngdoc directive
+	 * @name commons.ui.msmMobileMenuItem:msmMobileMenuItem
+	 * @scope
+	 * @restrict 'E'
+	 * @element ANY
+	 *
+	 * @description Directive that renders a menu point for the mobile view with an icon, name, preview and link. You can
+	 * apply your own ng-click directive to it or use the targetState param to handle click actions.
+	 *
+	 * @param {string} targetState (optional) the string that identifies the state to go to
+	 * @param {string} name the string that reperesents the message key for the target
+	 * @param {string} preview the value to be previewed
+	 * @param {string} icon the classes for the icon
+	 */
+	angular
+		.module('msm.components.ui')
+		.directive('msmMobileMenuItem', MobileMenuItem);
+
+	function MobileMenuItem($state, $translate, $timeout) {
+		return {
+			templateUrl: 'components/ui/msm-mobile-menu-item/msm-mobile-menu-item.html',
+			scope: {
+				previewValue: '=',
+				targetState: '=',
+				labelText: '=',
+				icon: '='
+			},
+			controller: function($scope) {
+
+				$scope.open = function() {
+					if($scope.targetState) {
+						$state.go($scope.targetState);
+					}
+				};
+
+        $translate($scope.labelText).then(function(translatedValue) {
+          $scope.labelText = translatedValue;
+        });
+			}
+		};
+	}
+})();
+
 (function () {
   'use strict';
 
@@ -534,53 +581,6 @@ angular.module('msm.components.ui')
     };
   }
 })();
-(function() {
-	'use strict';
-
-	/**
-	 * @ngdoc directive
-	 * @name commons.ui.msmMobileMenuItem:msmMobileMenuItem
-	 * @scope
-	 * @restrict 'E'
-	 * @element ANY
-	 *
-	 * @description Directive that renders a menu point for the mobile view with an icon, name, preview and link. You can
-	 * apply your own ng-click directive to it or use the targetState param to handle click actions.
-	 *
-	 * @param {string} targetState (optional) the string that identifies the state to go to
-	 * @param {string} name the string that reperesents the message key for the target
-	 * @param {string} preview the value to be previewed
-	 * @param {string} icon the classes for the icon
-	 */
-	angular
-		.module('msm.components.ui')
-		.directive('msmMobileMenuItem', MobileMenuItem);
-
-	function MobileMenuItem($state, $translate, $timeout) {
-		return {
-			templateUrl: 'components/ui/msm-mobile-menu-item/msm-mobile-menu-item.html',
-			scope: {
-				previewValue: '=',
-				targetState: '=',
-				labelText: '=',
-				icon: '='
-			},
-			controller: function($scope) {
-
-				$scope.open = function() {
-					if($scope.targetState) {
-						$state.go($scope.targetState);
-					}
-				};
-
-        $translate($scope.labelText).then(function(translatedValue) {
-          $scope.labelText = translatedValue;
-        });
-			}
-		};
-	}
-})();
-
 (function () {
   'use strict';
 
