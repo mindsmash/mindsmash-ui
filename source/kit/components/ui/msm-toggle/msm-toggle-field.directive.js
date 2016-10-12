e9ed5b6718318b3824a42e95bab765c073e3941c
Fynn Feldpausch <fynn.feldpausch@mindsmash.com>
2016-10-12 16:47:53 +0200

    Fixed update bug in msm-toggle-field

--- source/kit/components/ui/msm-toggle/msm-toggle-field.directive.js
+++ source/kit/components/ui/msm-toggle/msm-toggle-field.directive.js
@@ -10,17 +10,24 @@
       require: '^^msmToggleForm',
       link: function(scope, elem, attrs) {
         elem.addClass('msm-toggle-show');
+
         var content = $compile('<div class="form-control-static msm-toggle-hide"></div>')(scope);
         elem.after(content);
-        scope.$on('msmToggleForm:isEditable', function(event, isEditable) {
+        setContent();
+
+        scope.$on('$destroy', scope.$on('msmToggleForm:isEditable', function(event, isEditable) {
           if (!isEditable) {
-            if (attrs.msmToggleField) {
-              content.html($sanitize(scope.$eval(attrs.msmToggleField)));
-            } else {
-              content.text(scope.$eval(attrs.ngModel));
-            }
+            setContent();
           }
-        });
+        }));
+
+        function setContent() {
+          if (attrs.msmToggleField) {
+            content.html($sanitize(scope.$eval(attrs.msmToggleField)));
+          } else {
+            content.text(scope.$eval(attrs.ngModel));
+          }
+        }
       }
     }
   }
